c584a9244324023bae0153c060a7f4c65ce6eca1
fanadape <fanadape@gmail.com>
2023-09-16 10:41:54 +0300

    feat: add array with page-path

--- src/components/base/header/TheHeader.vue
+++ src/components/base/header/TheHeader.vue
@@ -2,11 +2,13 @@
   <header class="header">
     <div class="header__inner container">
       <BaseLogo />
+      <TheHeaderList />
     </div>
   </header>
 </template>
 
 <script setup lang="ts">
+import TheHeaderList from '@/components/base/header/TheHeaderList.vue'
 import BaseLogo from '@/components/base/logo/BaseLogo.vue'
 </script>
 
---
+++ src/components/base/header/TheHeaderList.vue
@@ -0,0 +1,34 @@
+<template>
+  <ul class="header-list">
+    <li class="header-list__item"></li>
+    <li class="header-list__item" v-for="{ nameItem, pathItem } in ListMenu" :key="nameItem">
+      <router-link class="" :to="pathItem">
+        {{ nameItem }}
+      </router-link>
+    </li>
+  </ul>
+</template>
+
+<script setup lang="ts">
+interface itemListMenu {
+  nameItem: string
+  pathItem: string
+}
+
+const ListMenu: itemListMenu[] = [
+  {
+    nameItem: 'On Sale',
+    pathItem: '/OnSale'
+  },
+  {
+    nameItem: 'New Arrivals',
+    pathItem: '/NewArrivals'
+  },
+  {
+    nameItem: 'Brands',
+    pathItem: '/Brands'
+  }
+]
+</script>
+
+<style scoped></style>
